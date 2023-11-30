f54a3e1f72206941d9bef28a3b23b204aa6ac0ae
SHESHKUMAR95 <148970064+SHESHKUMAR95@users.noreply.github.com>
2023-11-30 12:56:42 +0530

    Initial commit

---
+++ 2Operation_Subtraction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Operation_Subtraction
+{
+    internal class Program
+    {
+        public void Subtraction()
+        {
+            int a = 20, b = 10;
+            int c = a - b;
+            Console.WriteLine("Subtraction : " + c);
+        }
+        static void Main(string[] args)
+        {
+            Program op = new Program();
+            op.Subtraction();
+            Console.ReadLine(); 
+        }
+    }
+}
